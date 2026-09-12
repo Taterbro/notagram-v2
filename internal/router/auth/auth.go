89d5adac13fab12d89e754ba8350988cdf631876
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-09-12 19:49:02 +0100

    Update auth.go

--- internal/router/auth/auth.go
+++ internal/router/auth/auth.go
@@ -18,4 +18,5 @@ func RegisterRoutes(r *gin.RouterGroup, cfg *config.Config, db *sql.DB, redis Re
 	r.POST("/logout", middleware.RequireTokenType(cfg, middleware.Refresh, redis), h.Logout)
 	r.POST("/refresh", middleware.RequireTokenType(cfg, middleware.Refresh, redis), h.Refresh)
 	r.POST("/password/change", middleware.RequireTokenType(cfg, middleware.Access, redis), h.UpdatePassword)
+	r.POST("/password/", middleware.RequireTokenType(cfg, middleware.Access, redis), h.UpdatePassword)
 }
